2a95e081ca6adfb4cef57c059b01d3955623b04a
grzegorz <grzegorz@polarbits.co>
2018-01-10 16:01:10 +0100

    add react-router

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+  } from 'react-router-dom';
+  
+import Login from './Login.jsx';
+import Register from './Register.jsx';
+
 
 export default class App extends React.Component {
     render() {
         return (
-            <div style={{textAlign: 'center'}}>
-            <h1>Hello Worlds </h1>
-            </div>
+        <Router>
+        <div className="container">
+            <ul>
+            <li>
+              <Link to="/">Login</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+          </ul>
+
+          <Route exact path="/" component={Login} />
+          <Route path="/register" component={Register} />
+        </div>
+          </Router>
+            // <div className="container" style={{textAlign: 'center'}}>
+            // <h1>Hello Worlds </h1>
+            // </div>
         );
     }
 }
--- client/index.js
+++ client/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
-import Login from './components/Login.jsx';
-import Register from './components/Register.jsx';
 
-ReactDOM.render(<Login />, document.getElementById('root'));
-ReactDOM.render(<Register />, document.getElementById('registerForm'));
+ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<Login />, document.getElementById('root'));
+// ReactDOM.render(<Register />, document.getElementById('registerForm'));
